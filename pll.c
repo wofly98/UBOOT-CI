07c7f8119cbf64850d9296bada49d13483fdf95b
fangyuwu@weflywifi.com <fangyuwu@weflywifi.com>
2025-03-14 09:48:39 +0800

    change to 800MHz

--- pll.c
+++ pll.c
@@ -94,11 +94,11 @@ void mtk_pll_init(int skip_dcm_setting)
 	mmio_clrbits_32(MPLL_PWR_CON0, CON0_ISO_EN);
 
 	/* Set PLL frequency */
-	mmio_write_32(ARMPLL_CON1, 0x82000000); /* 1.3G */
+	mmio_write_32(ARMPLL_CON1, 0x50000000); /* 1.3G */
 
-	mmio_setbits_32(ARMPLL_CON0, 0x124); /* divider for 650M */
+	//mmio_setbits_32(ARMPLL_CON0, 0x124); /* divider for 650M */
 
-	//mmio_setbits_32(ARMPLL_CON0, 0x114); /* divider for 1.3G */
+	mmio_setbits_32(ARMPLL_CON0, 0x114); /* divider for 1.3G */
 
 	mmio_setbits_32(NET2PLL_CON0, 0x114);
 	mmio_setbits_32(MMPLL_CON0, 0x124);
